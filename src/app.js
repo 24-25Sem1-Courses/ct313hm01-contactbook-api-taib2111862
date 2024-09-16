5eb85ea8a7d0e5f4c6849d921f7f50be892c91ff
taib2111862 <155361204+taib2111862@users.noreply.github.com>
2024-09-17 00:27:31 +0700

    Standardize JSON responses

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
+const JSend = require('./jsend');
 const contactsRouter = require('./routes/contacts.router');
 const app = express();
 
@@ -9,9 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    return res.json({
-        message: 'ok'
-    });
+    return res.json(JSend.success());
 });
 
 contactsRouter.setup(app);
